5740e0aa683fab95696b3589d942375382be52f3
ThomasV <thomasv@electrum.org>
2018-09-23 17:43:51 +0200

    add lnworker in start_network

--- electrum/wallet.py
+++ electrum/wallet.py
@@ -63,6 +63,7 @@ from .contacts import Contacts
 from .interface import RequestTimedOut
 from .ecc_fast import is_using_fast_ecc
 from .mnemonic import Mnemonic
+from .lnworker import LNWorker
 
 if TYPE_CHECKING:
     from .network import Network
@@ -219,6 +220,10 @@ class Abstract_Wallet(AddressSynchronizer):
         self.contacts = Contacts(self.storage)
 
         self._coin_price_cache = {}
+
+    def start_network(self, network):
+        AddressSynchronizer.start_network(self, network)
+        self.lnworker = LNWorker(self, network)
 
     def load_and_cleanup(self):
         self.load_keystore()
