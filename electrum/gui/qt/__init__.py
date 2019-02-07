570ec4e321ce5bd1cbbc11afafb313e8984b8b75
Janus <ysangkok@gmail.com>
2019-02-07 18:15:34 +0100

    lightning: post-rebase fixes, read_QIcon and invalid import

--- electrum/gui/qt/__init__.py
+++ electrum/gui/qt/__init__.py
@@ -50,9 +50,6 @@ from electrum.util import (UserCancelled, PrintError, profiler,
 from electrum.wallet import Wallet, Abstract_Wallet
 
 from .installwizard import InstallWizard, WalletAlreadyOpenInMemory
-
-from electrum.lightning import LightningUI
-
 from .util import get_default_language, read_QIcon, ColorScheme
 from .main_window import ElectrumWindow
 from .network_dialog import NetworkDialog
